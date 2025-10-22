6ee205b29cf85f6e3537be898d7d9153c1468214
plms <wuyangzerg@gmail.com>
2025-10-22 10:34:17 +0800

    pingpong is completed

--- user/pingpong.c
+++ user/pingpong.c
@@ -3,27 +3,51 @@
 
 int main(int argc, char *argv[])
 {
-	int n, pid, c_pid;
+	int pid, c_pid;
 	int fds[2];
-	char buff[10];
+	int fds_b[2];
+	char buff[2];
 	
 	pipe(fds);
+	pipe(fds_b);
+
 	c_pid = fork();
 	if (c_pid == 0) {
+		// A child process reads a byte from a pipe and stores the data to "buff".
+		read(fds[0], buff, 1);
 
-		pid = getpid();
-		write(fds[1], "ping\n", 5);
-		printf("child's pid: %d\n", pid);
+		if (buff[0] == 'A') {
+			// If a child received "A" from its parent, it writes the "A" 
+			// into its file descriptor. Since the default output of a process 
+			// is a console, so the "A" will be printed on the CLI.
+			//write(1, buff, 1); // To test.
+
+			pid = getpid();
+			printf("%d: received ping\n", pid);
+
+			// The child should write "A" back the a pipe so that its parent can
+			// read from the pipe.
+			write(fds_b[1], "B", 1);
+			exit(0);
+		}
+
+		exit(1);
+
 
 	} else {
 
-		n = read(fds[0], buff, sizeof(buff));
-		write(1, buff, n);
+		write(fds[1], "A", 1);
 
-		pid = getpid();
+		// Wait for a child process to exit.
+		wait(0);
+		
+		read(fds_b[0], buff, 1);
+		if (buff[0] == 'B') {
+				pid = getpid();
+				printf("%d: received pong\n", pid);
+		}
+		exit(0);
 
-		printf("In parent process, child's pid: %d\n", c_pid);
-		printf("parent's pid: %d\n", pid);
 	}
 
 
