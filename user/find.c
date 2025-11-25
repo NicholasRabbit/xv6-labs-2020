a505b732ca065bbc9b96e9aa24319043b9a5f0a2
plms <wuyangzerg@gmail.com>
2025-11-25 10:22:50 +0800

    part of the lab of find solved; have not recursive yet

--- user/find.c
+++ user/find.c
@@ -3,21 +3,42 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 
-int display_names(char *path, char *name)
+int find(char *path, char *name)
 {
-	//char buff[512], *p;
+	char buf[512], *p;
 	int fd;
-	//struct dirent de;
+	struct dirent de;
 	struct stat st;
 	
 	// Open the current directory and get its file descriptor.
 	fd = open(path, 0);
 
-	// "fstat" is a system call populating infomation about an open
-	// file into *st.
+	/*
+	 * "fstat" is a system call populating infomation about an open
+	 * file into *st.
+	 */ 
 	fstat(fd, &st);
+	
+	strcpy(buf, path);
+	p = buf + strlen(buf);
+	*p++ = '/'; 
 
-	printf("st.size = %d\n", st.size);
+	while(read(fd, &de, sizeof(de)) == sizeof(de)) {
+		if ((de.inum) == 0)
+			continue;
+		// Concact the name of a file to the address of "p".
+		memmove(p, de.name, DIRSIZ);
+		p[DIRSIZ] = 0; // Add 0(NULL) to indicate the end of a string.
+		// Note that the return value is NOT 0 when two strings are NOT equal.
+		if(!strcmp(name, p))
+			printf("%s\n", p);
+		
+	
+	}
+
+	close(fd);
+
+	exit(0);
 
 
 	return 0;
@@ -25,16 +46,16 @@ int display_names(char *path, char *name)
 
 int main(int argc, char *argv[])
 {
-		if (argc < 2) {
-			fprintf(2, "find: without the name of file.");
-			exit(2);
-		}
-		if (argc > 2) {
-			fprintf(2, "find: No more than two arguments, please");
+	if (argc < 2) {
+		fprintf(2, "find: without the name of file.");
+		exit(2);
+	}
+	if (argc > 2) {
+		fprintf(2, "find: No more than two arguments, please");
 	}
 
 	// For now, we find current directory by default. 
-	display_names(".", argv[1]);
+	find(".", argv[1]);
 
 
 	return 0;
